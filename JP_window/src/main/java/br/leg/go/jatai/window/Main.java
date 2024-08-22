37493b26a4656933b4f2304727d4fff7592ae223
jpsri <jpsri@Shelby>
2024-08-22 16:20:36 -0300

    initial commit

---
+++ JP_window/src/main/java/br/leg/go/jatai/window/Main.java
@@ -0,0 +1,16 @@
+package br.leg.go.jatai.window;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author jpsri
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       Window window = new Window();
+    }
+}
---
+++ JP_window/src/main/java/br/leg/go/jatai/window/Window.java
@@ -0,0 +1,31 @@
+
+package br.leg.go.jatai.window;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+
+public class Window extends JFrame implements ActionListener {
+    
+    Window(){
+        
+      this.setTitle("Cadastro de Clientes");
+    this.setSize(400,200);
+    this.setLocationRelativeTo(null);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    this.setVisible(true);
+   
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+        
+        
+    
+}
